96f5cbd609359a872372ea7a7505f979887ccdc9
Christian Heinemann <ch@chlab.net>
2012-02-24 10:52:32 +0100

    Error handling in Message Driven Bean improved

--- emailsender-ejb/src/main/java/net/chlab/emailsender/ejb/EmailSenderMessageBean.java
+++ emailsender-ejb/src/main/java/net/chlab/emailsender/ejb/EmailSenderMessageBean.java
@@ -7,8 +7,8 @@ import java.util.logging.Logger;
 
 import javax.annotation.Resource;
 import javax.ejb.ActivationConfigProperty;
+import javax.ejb.EJBException;
 import javax.ejb.MessageDriven;
-import javax.ejb.MessageDrivenContext;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
@@ -25,56 +25,43 @@ import javax.mail.internet.MimeMessage;
 public class EmailSenderMessageBean implements MessageListener {
 
 	private static Logger logger = Logger.getLogger(EmailSenderMessageBean.class.getName());
-	
-	@Resource
-	private MessageDrivenContext mdc;
-	
-	@Resource(mappedName="mail/james")
+
+	@Resource(mappedName = "mail/james")
 	private Session session;
 
 	public void onMessage(Message inMessage) {
-		TextMessage msg = null;
-
-		try {
-			if (inMessage instanceof TextMessage) {
-				msg = (TextMessage) inMessage;
-				System.out.println("MESSAGE BEAN: Message received: " + msg.getText());
+		if (inMessage instanceof TextMessage) {
+			final TextMessage msg = (TextMessage) inMessage;
+			try {
+				logger.log(Level.INFO, "MESSAGE BEAN: Message received: " + msg.getText());
 				sendMail("christian.heinemann@nb058.saxsys.de", msg.getText());
-			} else {
-				System.out.println("Message of wrong type: " + inMessage.getClass().getName());
+			} catch (JMSException ex) {
+				throw new EJBException("JMS error", ex);
 			}
-		} catch (JMSException e) {
-			e.printStackTrace();
-			mdc.setRollbackOnly();
-		} catch (Throwable te) {
-			te.printStackTrace();
+		} else {
+			throw new EJBException("Message of wrong type: " + inMessage.getClass().getName());
 		}
+
 	}
-	
+
 	private void sendMail(String recipient, String subject) {
 		try {
 			final MimeMessage message = new MimeMessage(session);
-	        message.setFrom();
-	        message.setRecipients(MimeMessage.RecipientType.TO,
-	                InternetAddress.parse(recipient, false));
-	        message.setSubject(subject);
-	        message.setHeader("X-Mailer", "JavaMail");
-	        DateFormat dateFormatter = DateFormat
-	                .getDateTimeInstance(DateFormat.LONG, DateFormat.SHORT);
-	        Date timeStamp = new Date();
-	        String messageBody = "This is a test message from the async example "
-	                + "of the Java EE Tutorial. It was sent on "
-	                + dateFormatter.format(timeStamp)
-	                + ".";
-	        message.setText(messageBody);
-	        message.setSentDate(timeStamp);
-	        Transport.send(message);
-	        logger.log(Level.INFO, "Mail sent to {0}", recipient);
-	    } catch (MessagingException ex) {
-	        logger.severe("Error in sending message.");
-	        logger.severe(ex.getMessage() + ex.getNextException().getMessage());
-	        logger.severe(ex.getCause().getMessage());
-	    }
+			message.setFrom();
+			message.setRecipients(MimeMessage.RecipientType.TO, InternetAddress.parse(recipient, false));
+			message.setSubject(subject);
+			message.setHeader("X-Mailer", "JavaMail");
+			DateFormat dateFormatter = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.SHORT);
+			Date timeStamp = new Date();
+			String messageBody = "This is a test message from the async example "
+					+ "of the Java EE Tutorial. It was sent on " + dateFormatter.format(timeStamp) + ".";
+			message.setText(messageBody);
+			message.setSentDate(timeStamp);
+			Transport.send(message);
+			logger.log(Level.INFO, "Mail sent to {0}", recipient);
+		} catch (MessagingException ex) {
+			throw new EJBException("Error in sending email.", ex);
+		}
 	}
 
 }
